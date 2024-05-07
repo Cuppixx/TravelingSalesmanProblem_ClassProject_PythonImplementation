9d80e137eeb006cb21a92d5db2cf9f9d58af2ab6
Cuppixx <CuppiXD@gmx.de>
2024-05-07 20:14:03 +0200

    Implemented SA. Filled out ResultSet1

--- tsp.py
+++ tsp.py
@@ -1,20 +1,16 @@
 from itertools import permutations
-from random import shuffle
 import math
 import random
 import time
 
-# load the distnace matrix from file
 def loadDistances():
 	distancesFile = open("citiesAndDistances.txt")
 	cities = distancesFile.readline().split()
 	del cities[16:]
 	numCities = len(cities)
 	
-	# create an empty 2-dimensional matrix
 	cityDistances = [[0.0] * numCities for i in range(numCities)]
 	
-	# for all cities:
 	for i in range(numCities):
 		distances = distancesFile.readline().split()
 		del distances[0]
@@ -23,46 +19,29 @@ def loadDistances():
 			cityDistances[i][j] = int(distances[j])
 	return (cities,cityDistances)
 
-# compute for a permutation of city names p the trip length
-# compute first the distance form the last to the first city
-# then add the distance from the first to the second city,...
-# and finally, add the distance from the second last to the last city
-#
 def measurePath(p, distances):
 	length = 0
 	for i in range(len(p)):
 		length += distances[p[i-1]][p[i]]
 	return length
 
-# example implementation of the random walk algorithm
-# consider only cities from 0 to maximalNoOfCities
 def random_walk(maximalNoOfCities, iterations = 1000000):
-	
-	# get the longest trip length for an upper bound
-	# sum up all distances of a 2-dimensional list
+
 	shortestTourLength = sum([sum(i) for i in distances])
 	
-	# for one million steps do: randomly sample a tour and compute the 
-	# length. if better tour found, remember it
 	for j in range(iterations):
-		
-		# sample random permutation
 		p=list(range(maximalNoOfCities))
-		shuffle(p)
+		random.shuffle(p)
 		
-		# measure path length
 		tourLength = measurePath(p, distances)
 		
-		# if new path is shorter than the old best path, remember the new path
 		if tourLength < shortestTourLength:
 			shortestTourLength = tourLength
 			shortestTour = p
 			
-			#print the new shortest path
 			shortestTourCities = [cities[i] for i in shortestTour]
 			print("iteration: {} tour: {} length: {}".format(j,shortestTourCities, shortestTourLength))
-		
-			# do some status printing from time to time
+               
 		elif j % 100000 == 0:
 			print('iteration: {}'.format(j))	
 
@@ -72,25 +51,22 @@ def brute_force(maximalNoOfCities):
     shortest_tour_length = float('inf')
     shortest_tour = None
     
-    # Generate all permutations of city indices
     for perm in permutations(range(num_cities)):
         tour_length = measurePath(perm, distances)
-        #print("Tour found by brute force: {} with length: {}".format([cities[i] for i in perm], tour_length))
-        # Update shortest tour if the current tour is shorter
+        # Print Statement should be commented out during measurement
+        print("Tour found by brute force: {} with length: {}".format([cities[i] for i in perm], tour_length))
         if tour_length < shortest_tour_length:
             shortest_tour_length = tour_length
             shortest_tour = perm
             
     shortest_tour_cities = [cities[i] for i in shortest_tour]
-    print("Shortest tour found by brute force: {} with length: {}".format(shortest_tour_cities, shortest_tour_length))
     time_end = time.perf_counter()
-    print("Got the optimal Tour for {} cities in {} seconds".format(maximalNoOfCities,time_end - time_start))	
+    print("Shortest tour found by brute force: {} with length: {}".format(shortest_tour_cities, shortest_tour_length))
+    print("Got the optimal Tour for {} cities in {} seconds".format(maximalNoOfCities, time_end - time_start))	
 
 def two_opt_xchg(tour, i, k):
-    #print(tour)
-    new_tour = tour[:i] + list(reversed(tour[i:k+1])) + tour[k+1:]
-    #print(new_tour)
-    return new_tour
+    xchg_tour = tour[:i] + list(reversed(tour[i:k+1])) + tour[k+1:]
+    return xchg_tour
 
 def two_opt_step(tour, distances):
     num_cities = len(tour)
@@ -109,19 +85,17 @@ def two_opt_step(tour, distances):
                     shortest_length = new_length
                     tour = new_tour
                     improvement = True
-                    break  # Start inner loop again with the updated tour
-            if improvement:
-                break  # Start outer loop again with the updated tour
+                    break
+            if improvement: break
     
     return tour, shortest_length, evaluated_lengths
 
 def hill_climber(distances, maximalNoOfCities):
-    random_tour = list(range(maximalNoOfCities))
-    #random_tour = list(range(len(distances)))
-    random.shuffle(random_tour)
-    #random_tour = random_tour[0:maximalNoOfCities]
-    print("Random Tour: {}".format(random_tour))
     start_time = time.perf_counter()
+
+    random_tour = list(range(maximalNoOfCities))
+    random.shuffle(random_tour)
+    print("Random Tour: {}".format(random_tour))
     
     current_tour = random_tour
     current_length = measurePath(current_tour, distances)
@@ -129,53 +103,53 @@ def hill_climber(distances, maximalNoOfCities):
     
     while True:
         improved = False
-        new_tour, new_length, evals = two_opt_step(current_tour, distances)
-        evaluated_lengths += evals
+        new_tour, new_length, evaluations = two_opt_step(current_tour, distances)
+        evaluated_lengths += evaluations
         if new_length < current_length:
             current_tour = new_tour
             current_length = new_length
             improved = True
-        if not improved:
-            break
+        if not improved: break
     
     end_time = time.perf_counter()
     computation_time = end_time - start_time
-    
     return current_length, evaluated_lengths, computation_time
 
 def compute_with_2opt(maximalNoOfCities):
-    # Load the distance matrix and city names
     (cities, distances) = loadDistances()
     
-    # Perform 2-opt hill climber for the given number of cities
-    tour_length, evaluated_lengths, computation_time = hill_climber(distances, maximalNoOfCities)
-    
-    print(f"For {maximalNoOfCities} cities:")
-    print(f"Shortest tour length found by 2-opt hill climber: {tour_length}")
-    print(f"Number of evaluated tour lengths: {evaluated_lengths}")
-    print(f"Computation time: {computation_time} seconds\n\n")
+    evaluated_lengths_array = []
+    computation_times_array = []
+    for n in range(5):
+        tour_length, evaluated_lengths, computation_time = hill_climber(distances, maximalNoOfCities)
+        evaluated_lengths_array.append(evaluated_lengths)
+        computation_times_array.append(computation_time)
+        print(f"For {maximalNoOfCities} cities:")
+        print(f"Shortest tour length found by 2-opt hill climber: {tour_length}")
+        print(f"Number of evaluated tour lengths: {evaluated_lengths}")
+        print(f"Computation time: {computation_time} seconds\n\n")
 
-def node_insertion(tour):
-     #print(tour)
-     random_node = random.randint(0,len(tour)-1)
-     #print(random_node)
-     random_indx = random_node
-     while random_indx == random_node:
-        random_indx = random.randint(0,len(tour)-1)
-        #print(random_indx)
-     tour.pop(random_node)
-     tour.insert(random_indx,random_node)
-     return tour
+    average_evaluated_lengths = sum(evaluated_lengths_array) / len(evaluated_lengths_array)
+    average_computation_time = sum(computation_times_array) / len(computation_times_array)
+    print(f"Average evaluated tour lengths: {average_evaluated_lengths}")
+    print(f"Average computation time: {average_computation_time} seconds")
 
 def node_exchange(tour):
      random_node1 = random.randint(0,len(tour)-1)
-     #print(random_node)
      random_node2 = random_node1
-     while random_node2 == random_node1:
-        random_node2 = random.randint(0,len(tour)-1)
+     while random_node2 == random_node1: random_node2 = random.randint(0,len(tour)-1)
      temp=tour[random_node1]
      tour[random_node1]=tour[random_node2]
      tour[random_node2]=temp
+     return tour
+
+def node_insertion(tour):
+     random_node1 = random.randint(0,len(tour)-1)
+     random_node2 = random_node1
+     while random_node2 == random_node1: random_node2 = random.randint(0,len(tour)-1)
+     node_value = tour[random_node1]
+     tour.pop(random_node1)
+     tour.insert(random_node2,node_value)
      return tour
 
 def simulated_annealing(distances, maximalNoOfCities, initial_temperature=1000, cooling_rate=0.999, stopping_temperature=0.01):
@@ -186,11 +160,33 @@ def simulated_annealing(distances, maximalNoOfCities, initial_temperature=1000, 
     current_length = measurePath(current_tour, distances)
     temperature = initial_temperature
     evaluated_lengths = 0
+
     start_time = time.perf_counter()
 
+    exchange = 0
+    insertion = 0
+    xchg = 0
+    
     while temperature > stopping_temperature:
-        new_tour, new_length, evals = two_opt_step(current_tour, distances)
-        evaluated_lengths += evals
+        #print(f"Temperature: {temperature}")
+        perturbation_operator = random.randint(0,2)
+        match perturbation_operator:
+             case 0:
+                  exchange += 1 
+                  new_tour = node_exchange(list(current_tour))
+             case 1: 
+                  insertion += 1
+                  new_tour = node_insertion(list(current_tour))
+             case 2:
+                  xchg += 1
+                  i = random.randint(0,len(current_tour)-2)
+                  k = random.randint(i+1,len(current_tour)-1) 
+                  new_tour = two_opt_xchg(list(current_tour), i, k)
+             case _: new_tour = node_exchange(list(current_tour))
+        
+        new_length = measurePath(new_tour, distances)
+
+        evaluated_lengths += 1
         delta_length = new_length - current_length
         if delta_length < 0 or random.random() < math.exp(-delta_length / temperature):
             current_tour = new_tour
@@ -199,34 +195,38 @@ def simulated_annealing(distances, maximalNoOfCities, initial_temperature=1000, 
 
     end_time = time.perf_counter()
     computation_time = end_time - start_time
-
+    print(f"--Simmulated Annealing--\nPicked Exchange {exchange} times\nPicked Insertion {insertion} times\nPicked xchg {xchg} times")
     return current_length, evaluated_lengths, computation_time
 
-
-def compute_with_simulated_annealing(maximalNoOfCities, initial_temperature=1000, cooling_rate=0.999, stopping_temperature=0.01):
+def compute_with_simulated_annealing(maximalNoOfCities):
     cities, distances = loadDistances()
-    tour_length, evaluated_lengths, computation_time = simulated_annealing(distances, maximalNoOfCities, initial_temperature, cooling_rate, stopping_temperature)
-    print(f"For {maximalNoOfCities} cities:")
-    print(f"Shortest tour length found by Simulated Annealing: {tour_length}")
-    print(f"Number of evaluated tour lengths: {evaluated_lengths}")
-    print(f"Computation time: {computation_time} seconds\n\n")  
+
+    evaluated_lengths_array = []
+    computation_times_array = []
+    for n in range(5):
+        tour_length, evaluated_lengths, computation_time = simulated_annealing(distances, maximalNoOfCities)
+        evaluated_lengths_array.append(evaluated_lengths)
+        computation_times_array.append(computation_time)
+        print(f"For {maximalNoOfCities} cities:")
+        print(f"Shortest tour length found by Simulated Annealing: {tour_length}")
+        print(f"Number of evaluated tour lengths: {evaluated_lengths}")
+        print(f"Computation time: {computation_time} seconds\n\n")  
+
+    average_evaluated_lengths = sum(evaluated_lengths_array) / len(evaluated_lengths_array)
+    average_computation_time = sum(computation_times_array) / len(computation_times_array)
+    print(f"Average evaluated tour lengths: {average_evaluated_lengths}")
+    print(f"Average computation time: {average_computation_time} seconds")
 
 #MAIN/////////////////////////////////////////////////////////////////////////
 maximalNoOfCities=int(input("Enter the maximal number of cities: "))
 
-# load the distance matrix and city names
 (cities, distances) = loadDistances()
-
-# print city names
 #print(cities[:maximalNoOfCities])
-
-# print distance matrix
 #print(distances)    
 
-#random_walk(maximalNoOfCities)
+#random_walk()
 #brute_force(maximalNoOfCities)
-for n in range(5):
-#    compute_with_2opt(maximalNoOfCities)
-    compute_with_simulated_annealing(maximalNoOfCities)
+#compute_with_2opt(maximalNoOfCities)
+compute_with_simulated_annealing(maximalNoOfCities)
 
 
